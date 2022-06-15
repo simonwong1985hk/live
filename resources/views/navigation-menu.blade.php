432ff736412ea94a24d8807000d65baa21a45ca1
Simon Wong <simonwong1985hk@gmail.com>
2022-06-15 17:22:13 +0800

    navigation menu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('posts.index') }}" :active="request()->routeIs('posts.*')">
+                        {{ __('Post') }}
+                    </x-jet-nav-link>
                 </div>
             </div>
 
@@ -138,6 +141,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-jet-responsive-nav-link>
+            <x-jet-responsive-nav-link href="{{ route('posts.index') }}" :active="request()->routeIs('posts.*')">
+                {{ __('Post') }}
             </x-jet-responsive-nav-link>
         </div>
 
