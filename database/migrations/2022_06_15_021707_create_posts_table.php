0efe52f0750b5b85ba2e33ee8d946961b5926499
Simon Wong <simonwong1985hk@gmail.com>
2022-06-15 11:36:29 +0800

    database migrations

--- database/migrations/2022_06_15_021707_create_posts_table.php
+++ database/migrations/2022_06_15_021707_create_posts_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('body');
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
