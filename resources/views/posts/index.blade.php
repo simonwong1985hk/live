4690f7c7a0cf1f96517f110fe28aa80ed840a2cd
Simon Wong <simonwong1985hk@gmail.com>
2022-06-16 09:35:07 +0800

    read post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,52 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-
+                <!-- table from here -->
+                <div class="relative overflow-x-auto shadow-md">
+                    <table class="w-full text-sm text-left text-gray-500">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    ID
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Title
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Slug
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Body
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    <span class="sr-only">Edit</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($posts as $post)
+                            <tr class="bg-white border-b hover:bg-gray-50">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                    {{ $post->id }}
+                                </th>
+                                <td class="px-6 py-4">
+                                    {{ $post->title }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $post->slug }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    {{ $post->body }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    <a href="#" class="font-medium text-blue-600 hover:underline">Edit</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <!-- table to here -->
             </div>
         </div>
     </div>
