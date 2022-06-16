8ed6051dd26347bd121aee53651ace1cd1eed432
Simon Wong <simonwong1985hk@gmail.com>
2022-06-16 15:05:25 +0800

    delete post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -87,6 +87,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return redirect()->route('posts.index');
     }
 }
