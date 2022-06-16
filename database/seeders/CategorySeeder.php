92dc4da11279345559b02dc7aa0c1b0fc2557ecd
Simon Wong <simonwong1985hk@gmail.com>
2022-06-16 21:49:15 +0800

    create model using factory

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CategorySeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        Category::factory()->count(10)->create();
     }
 }
