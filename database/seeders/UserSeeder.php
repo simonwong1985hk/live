69f5b28eac357541dd0833d8b5f968dd80f18a5c
Simon Wong <simonwong1985hk@gmail.com>
2022-06-18 09:53:12 +0800

    update UserSeeder to handle many-to-many relationship

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -15,13 +16,29 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        // create an admin
+        User::factory()
+            ->hasAttached(Role::whereIn('name', ['admin'])->get())
+            ->create([
+                'name' => 'admin',
+                'email' => 'admin@example.com',
+                'password' => bcrypt('password'),
+            ]);
 
-        User::factory()->create([
-            'name' => 'admin',
-            'email' => 'admin@example.com',
-            'password' => bcrypt('password'),
-            'email_verified_at' => now(),
-        ]);
+        // create a user
+        User::factory()
+            ->hasAttached(Role::whereIn('name', ['user'])->get())
+            ->create([
+                'name' => 'user',
+                'email' => 'user@example.com',
+                'password' => bcrypt('password'),
+            ]);
+
+        // create some dummy users
+        User::factory(10)->create()->each(function ($user) {
+            $user->roles()->attach(
+                Role::pluck('id')->except(0)->random(),
+            );
+        });
     }
 }
