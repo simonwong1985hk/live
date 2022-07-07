678653fc1412d5f8d5f3c81dbcc556911c6d9bfd
Simon Wong <simonwong1985hk@gmail.com>
2022-07-07 23:52:55 +0800

    hot reload for blade files

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,17 @@ export default defineConfig({
             'resources/css/app.css',
             'resources/js/app.js',
         ]),
+        {
+            name: 'blade',
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith('.blade.php')) {
+                    server.ws.send({
+                        type: 'full-reload',
+                        path: '*',
+                    });
+                }
+            },
+        }
     ],
     server: {
         https: {
