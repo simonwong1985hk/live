3a4c3d4a8a73bf56a4059c362cebb124cb227393
Simon Wong <simonwong1985hk@gmail.com>
2022-07-12 09:46:38 +0800

    include flowbite

--- resources/views/components/frontend/layout.blade.php
+++ resources/views/components/frontend/layout.blade.php
@@ -13,6 +13,9 @@
         <!-- Font Awesome -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+        <!-- Flowbite Styles -->
+        <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.7/dist/flowbite.min.css" />
+
         <!-- Livewire Styles -->
         @livewireStyles
 
@@ -23,6 +26,9 @@
         <x-frontend.menu />
 
         {{ $slot }}
+
+        <!-- Flowbite Scripts -->
+        <script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
 
         <!-- Livewire Scripts -->
         @livewireScripts
