7616f05f488fa63878d5afd3c4a78183ad8b0552
Simon Wong <simonwong1985hk@gmail.com>
2022-07-16 10:30:20 +0800

    hot reload for arbitrary files

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,19 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-        ]),
-        {
-            name: 'blade',
-            handleHotUpdate({ file, server }) {
-                if (file.endsWith('.blade.php')) {
-                    server.ws.send({
-                        type: 'full-reload',
-                        path: '*',
-                    });
-                }
-            },
-        }
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: [
+                'app/Actions/**',
+                'app/Http/**',
+                'app/View/**',
+                'resources/views/**',
+                'routes/**',
+            ],
+        }),
     ],
     server: {
         https: true,
